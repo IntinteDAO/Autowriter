3b565c43c9e6d63ec48df87d86060096e6f5c316
fervi <lordfervi@protonmail.com>
2019-12-07 13:22:04 +0100

    Mining reward calculator

---
+++ Flippening/databases/reward.php
@@ -0,0 +1,17 @@
+<?php
+
+function reward($crypto) {
+$json = json_decode(file("https://howmanyconfs.com/api/data")[0], TRUE);
+
+for($i=0; $i<=count($json)-1; $i++) {
+	if($json[$i]['name']=='Bitcoin') { $price = $json[$i]['price']; }
+
+	if(strtolower($json[$i]['name'])==strtolower($crypto)) {
+
+		if($json[$i]['priceCurrency']=='BTC') { $price = $price*$json[$i]['price']; }
+		return number_format(floor(86400/$json[$i]['blockTimeInSeconds'])*$json[$i]['blockReward']*$price, 2, '.', '');
+	}
+
+}
+
+}
