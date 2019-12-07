11962501e2d7775bcacd3d08750bf65e76dc39a4
fervi <lordfervi@protonmail.com>
2019-12-07 14:06:56 +0100

    Fixed Marketcap function

--- Flippening/databases/marketcap.php
+++ Flippening/databases/marketcap.php
@@ -1,5 +1,5 @@
 <?php
 
 function marketcap($crypto) {
-return number_format(json_decode(file('https://api.coincap.io/v2/assets/'.$crypto)[0], TRUE)["data"]["priceUsd"], 2, '.', '');
+return number_format(json_decode(file('https://api.coincap.io/v2/assets/'.$crypto)[0], TRUE)["data"]["marketCapUsd"], 2, '.', '');
 }
