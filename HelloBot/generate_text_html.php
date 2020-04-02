5948744827a71d9b66b7de75c337a3420779b372
fervi <lordfervi@protonmail.com>
2020-04-02 17:24:05 +0200

    HelloBot: Finished example text generator engine

---
+++ HelloBot/generate_text_html.php
@@ -0,0 +1,46 @@
+<?php
+
+$lang = 'en';
+
+include('databases/fortunes.php');
+include('databases/hello.php');
+include('databases/weatherfunction.php');
+include('databases/date.php');
+include("lang/$lang/lang.php");
+include("config.php");
+
+// Hello
+echo hello('zapread', $lang);
+echo PHP_EOL.PHP_EOL;
+
+// Fortunes
+echo '<h2>'.$str['fortunes'].'</h2>'.PHP_EOL;
+echo fortunes($lang);
+echo PHP_EOL.PHP_EOL;
+
+// Special dates
+echo '<h2>'.$str['special_dates'].'</h2>'.PHP_EOL;
+if(date_newyear() == 0) { $new_year = '<b>'.$str['happynewyear'].'</b>'; } else { $new_year = date_newyear().' days remaining'; }
+if(date_holidays() == 0) { $holiday = '<b>'.$str['happyholidays'].'</b>'; } else { $holiday = date_holidays().' days remaining'; }
+if(date_santaclaus() == 0) { $santaclaus = '<b>'.$str['happysantaclaus'].'</b>'; } else { $santaclaus = date_santaclaus().' days remaining'; }
+
+echo '<table class="table table-hover table-striped table-sm">';
+echo '<tr><td>'.$str['date2newyear'].'</td><td>'.$new_year.'</td></tr>';
+echo '<tr><td>'.$str['date2holidays'].'</td><td>'.$holiday.'</td></tr>';
+echo '<tr><td>'.$str['date2santaclaus'].'</td><td>'.$santaclaus.'</td></tr>';
+echo '<tr><td>'.$str['date2craigwright'].'</td><td>'.date_craigwright().'</td></tr>';
+echo '</table>';
+echo PHP_EOL.PHP_EOL;
+// Weather
+echo '<h2>'.$str['weather'].'</h2>'.PHP_EOL;
+$weather_city = file('city.txt');
+
+echo '<table class="table table-hover table-striped table-sm">';
+for($i=0; $i<=count($weather_city)-1; $i++) {
+	if($i%2==0) {
+		echo '<tr><td>'.trim($weather_city[$i]).'</td><td>'.weather(trim($weather_city[$i])).'</td>';
+	} else {
+		echo '<td>'.trim($weather_city[$i]).'</td><td>'.weather(trim($weather_city[$i])).'</td></tr>';
+	}
+}
+echo '</table>';
---
+++ HelloBot/lang/en/lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$str['fortunes'] = 'Joke or quote:';
+$str['special_dates'] = 'Special dates:';
+$str['date2newyear'] = 'The time remaining until the new year';
+$str['happynewyear'] = 'Happy New Year!';
+$str['date2holidays'] = 'Time for summer holidays';
+$str['happyholidays'] = 'I detected a holiday! Have a nice rest and sunny weather!';
+$str['date2santaclaus'] = 'Time for Santa Claus';
+$str['happysantaclaus'] = 'Merry Christmas!';
+$str['date2craigwright'] = 'The time when Craig Faketoshi Wright proves he is Satoshi Nakamoto';
+$str['weather'] = 'Weather';
